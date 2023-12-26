c23d44e80455e9ad20469b1e9d2813d61e8b88c4
Dharmesh <dharmeshsaini777@gmail.com>
2023-12-26 23:16:43 +0530

    ram ram

--- Blog/settings.py
+++ Blog/settings.py
@@ -154,34 +154,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
